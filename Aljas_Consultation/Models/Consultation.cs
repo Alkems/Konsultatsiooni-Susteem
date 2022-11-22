6d32bd8c91280882565ba13462edc292d6d55d9a
Alkems <mynamezalter@gmail.com>
2022-11-22 15:24:25 +0200

    Add Consultation model and data inside it.

---
+++ Aljas_Consultation/Models/Consultation.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Aljas_Consultation.Models
+{
+    public class Consultation
+    {
+        // Id=1, Teacher=Mihkel, Classroom=215b, Day=Monaday, StartTime=15:00, EndTime=16:00, Period=First
+        public int Id { get; set; }
+        public string? Teacher { get; set; }
+        public int Classroom { get; set; }
+        public string? Day { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string? Period { get; set; }
+    }
+}
