03de68900b71a791621c3932f60c1cdbf9d59210
Alkems <mynamezalter@gmail.com>
2022-12-20 11:45:26 +0200

    Add SeedData.

---
+++ Aljas_Consultation/Data/SeedData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Aljas_Consultation.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Aljas_Consultation.Models;
+
+namespace Aljas_Consultation.Data
+{
+    public class SeedData : Controller
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<ApplicationDbContext>>()))
+            {
+                // Look for any Period.
+                if (context.Period.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Period.AddRange(
+                    new Period
+                    {
+                        Name = "First",
+                    },
+
+                    new Period
+                    {
+                        Name = "Second",
+                    },
+
+                    new Period
+                    {
+                        Name = "Third",
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
