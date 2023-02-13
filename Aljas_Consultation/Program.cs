fa7609d7c7adad8eacca58af6cd8226449446563
Alkems <Alkems@gmail.com>
2023-02-13 14:44:16 +0200

    Fix SeedData

--- Aljas_Consultation/Program.cs
+++ Aljas_Consultation/Program.cs
@@ -1,10 +1,26 @@
+using Aljas_Consultation.Data;
+
 namespace Aljas_Consultation
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
